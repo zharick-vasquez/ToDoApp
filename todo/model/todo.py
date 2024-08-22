acb0ef44abda2f092ecc1794fdbc4252e59fbe02
Thelastpepas <yonatancp.mgvc@gmail.com>
2024-08-22 11:38:33 -0500

    add comit

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -11,7 +11,27 @@ class Todo:
     def mark_completed(self):
         self.completed = True
 
-    def add_tag(self):
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+        else:
+            print("Ya esta en la lista")
+
+    def __str__(self):
+        return f"{self.code_id} - {self.title}"
+
+
+class TodoBook:
+    def __init__(self, todos):
+
+
+prueba = Todo(1, 'hh', 'jj')
+print(prueba.tags)
+prueba.add_tag('Sapoperro')
+print(prueba.tags)
+prueba.add_tag('Sapoperro')
+print(prueba.tags)
+
 
 
 
