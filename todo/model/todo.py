5e16e45513acfdc3b72e01b68d6f7b8e69761a43
Thelastpepas <yonatancp.mgvc@gmail.com>
2024-08-22 11:03:37 -0500

    add comit

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,20 @@
 # TODO: Add code here
+
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed = False
+        self.tags = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self):
+
+
+
+
+
+
