62dc442d84295e65d026c0e1e538e18b6950f37a
zharick <zvasquezvalenzuela@gmail.com>
2025-03-11 13:36:59 -0500

    add clases

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,30 @@
-# TODO: Add code here
+class Todo:
+    def __init__(self,code_id: int, title: str, description: str):
+        self.code_id: int = code_id
+        self.title: str = title
+        self.description: str = description
+        self.completed: bool = False
+        self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed: bool = True
+
+    def add_tag(self, tag):
+        if tag not in self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return f"{self.code_id} - {self.title}"
+
+class TodoBook:
+    def __init__(self):
+        self.todos:dict[int, Todo] = {}
+
+    def add_todo(self, title:str, description: str) -> int:
+        id = len(self.todos) + 1
+
+
+
+
+
+
